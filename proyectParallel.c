f453257387942afdde89055f52d11a99c31e787a
Augusto Alonso <augustoalonso98@gmail.com>
2022-03-18 00:47:43 -0600

    die

--- proyectParallel.c
+++ proyectParallel.c
@@ -24,13 +24,11 @@ double maxFromArray(double data[], int start, int end)
 {
     double inner_max = 0;
     int i;
-
+    // #pragma omp parallel for shared(data, end, start) private(i) reduction(max \
+//                                                                        : inner_max)
     for (i = start; i < end; i++)
     {
-        if (inner_max < data[i])
-        {
-            inner_max = data[i];
-        }
+        inner_max = inner_max > data[i] ? inner_max : data[i];
     }
     return inner_max;
 }
@@ -46,7 +44,6 @@ void sendTask(int start, int end, int num_threads, double temperatures2[], doubl
     double max = 0;
 
     // #pragma omp task
-    // printf("Task %d started with %d ended with %d\n", tid, start, end);
     for (int j = start; j < end; j++)
     {
         // if (j == end - 1)
@@ -67,7 +64,7 @@ int main(int argc, char *argv[])
     double t0 = 60.0;
     double tl = 100.0;
     double tr = 40.0;
-    int num_threads = 2;
+    int num_threads = 4;
     if (argc > 4)
     {
         num_threads = strtol(argv[1], NULL, 10);
@@ -92,38 +89,58 @@ int main(int argc, char *argv[])
         temperatures1[i] = t0;
     }
 
-    double start_time = omp_get_wtime();
     double max = 1;
     double new_max;
     int m;
-    int block = ceil((float)(N - 2) / (float)(num_threads));
+    int iterations = (N - 2);
+    int block = ceil((float)(iterations) / (float)(num_threads));
+
+    // if ((block * num_threads) < iterations)
+    // {
+    // block -= 1;
+    // }
     double differences[N];
     double differences_pt[num_threads];
     // printf("B size: %d", block);
+    double start_time = omp_get_wtime();
     omp_set_num_threads(num_threads);
-#pragma omp parallel shared(temperatures2, temperatures1, differences, max, new_max, block, num_threads, m, differences_pt)
+    omp_set_dynamic(0);
+    int end_l = (iterations - ((num_threads - 1) * block)) % iterations;
+#pragma omp parallel shared(temperatures2, temperatures1, differences, max, new_max, block, num_threads, m, differences_pt, iterations, end_l)
 #pragma omp single
     {
         while (max > ERR)
         {
             // Al threads create just one part of the block
-            for (m = 0; m < num_threads; m++)
+            for (m = 0; m < num_threads - 1; m++)
             {
-#pragma omp task shared(temperatures2, temperatures1, differences, block, num_threads, m, differences_pt)
+#pragma omp task shared(temperatures2, temperatures1, differences, block, num_threads, m, differences_pt, iterations, end_l)
                 {
-                    // int tid = omp_get_thread_num();
                     int start = m * block + 1;
-                    int end = start + (block - ((N - 2) % block));
+                    int end = start + block;
+                    // printf("Task %d started with %d ended with %d with b %d\n", m, start, end, block);
                     sendTask(start, end, num_threads, temperatures2, temperatures1, differences);
                     differences_pt[m] = maxFromArray(differences, start, end);
                 }
                 // printf("T%d done\n", tid);
             }
+#pragma omp task shared(temperatures2, temperatures1, differences, block, num_threads, differences_pt, iterations, end_l)
+            {
+
+                int start = (num_threads - 1) * block + 1;
+                int end = start + end_l;
+                // printf("Task %d started with %d ended with %d with b %d\n", m, start, end, block);
+                sendTask(start, end, num_threads, temperatures2, temperatures1, differences);
+                differences_pt[m] = maxFromArray(differences, start, end);
+            }
+            // printf("T%d done\n", tid);
+
 #pragma omp taskwait
 
             // printf("T%d started\n", 1);
             // TODO check how to make this parallell
             double new_max = maxFromArray(differences_pt, 0, num_threads);
+            // double new_max = maxFromArray(differences, 1, iterations);
             // printf("all done\n");
             memcpy(temperatures1, temperatures2, sizeof(temperatures1));
             // printf("Nuevo mayor: %lf\n", new_max);
